ab2eb7e1380a718ca91f9b393d4e38bb29d69136
py-38384 <piyal13133@gmail.com>
2021-12-10 08:40:48 +0600

    Commit

--- Web development/My websites/Foodsky/main.js
+++ Web development/My websites/Foodsky/main.js
@@ -1,10 +1,13 @@
 function change() {
   let element = document.getElementById("Navi");
   let eleback = element.style.display;
+  let body = document.querySelector('body');
   if (eleback == "flex") {
     element.style.display = "none";
+	body.style.overflow = "scroll";
   }
   else {
     element.style.display = "flex";
+	body.style.overflow = "hidden";
   }
 }
