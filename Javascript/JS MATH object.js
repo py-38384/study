b0ee0faf2537132cacc20490961edd38c93baa14
Piyal Hossain <piyal13133@gmail.com>
2022-06-25 18:03:17 +0600

    Commit

---
+++ Javascript/JS MATH object.js
@@ -0,0 +1,38 @@
+let x = 3;
+let y = 6;
+let z;
+z = x+y;
+z = x-y;
+z = x*y;
+z = x/y;
+
+// Exploring the Math object
+z = Math;
+z = Math.PI;
+z = Math.E;
+z = Math.round(5.4);
+z = Math.ceil(5.3);
+z = Math.floor(-5.3);
+z = Math.abs(5);
+z = Math.sqrt(64);
+z = Math.pow(2, 3);
+z = Math.min(2, 3, 34,234, 2342,34);
+z = Math.max(2, 3, 34,234,34);
+//random number 50 to 270
+z = Math.round(50+(Math.random()*(270-50)));
+// a = (0, 1)
+// a100 = a*100 = (0, 100)
+// a10_100 = 10+a*(100 - 10)
+
+console.log(z);
+
+// 3
+// 2.8
+// 2
+// 1
+// 0
+// -1
+// -2
+// -2.7
+// -3
+ 
