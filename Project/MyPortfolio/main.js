12544fb0b69fe6891324171de0b852d947e295b9
Piyal Hossein <piyal13133@gmail.com>
2023-05-21 00:13:55 +0600

    Commit

---
+++ Project/MyPortfolio/main.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".multiple-text",{
+    strings: ["Programmer","Youtuber","Web developer", "Game developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
