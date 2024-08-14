04a947f7efd346bddb4833346cd476e768eca938
Piyal Hossein <piyal13133@gmail.com>
2024-08-15 00:24:40 +0600

    updated command file

---
+++ Auth_JS/auth_js/app/ClientMember/page.jsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { getServerSession } from "next-auth"
+import { options } from "../api/auth/[...nextauth]/options"
+import { useSession } from 'next-auth/react'
+import { redirect } from "next/navigation"
+
+const ClientMember = () => {
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated(){
+      redirect('/api/auth/signin?callbackUrl=/ClientMember')
+    }
+  })
+  return (
+    <div>
+        <h1>Client Member Session</h1>
+        <p>{session?.user?.email}</p>
+        <p>{session?.user?.role}</p>
+    </div>
+  )
+}
+
+export default ClientMember
---
+++ Auth_JS/auth_js/app/CreateUser/page.jsx
@@ -0,0 +1,7 @@
+const CreateUser = () => {
+  return (
+    <div><h1>Only admins</h1></div>
+  )
+}
+
+export default CreateUser
